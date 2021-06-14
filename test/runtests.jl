cb08fdf4d6fee82a344df44c5907e7d284d272d2
MarkNahabedian <naha@mit.edu>
2021-06-14 15:16:56 -0400

    Test Allof and panel identity.)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,11 @@
+using Pkg
+Pkg.activate(".")
+
+using Revise
 using PanelCutting
+using Unitful
+using UnitfulUS
+using UnitfulCurrency
 using Test
 
 @testset "AllOf" begin
@@ -9,6 +16,75 @@ using Test
     @test collect(ao) == [1, 2, 3, :a, :b]
 end
 
+function allsubtypes(t, result=Set())
+    push!(result, t)
+    for t1 in subtypes(t)
+	allsubtypes(t1, result)
+    end
+    return result
+end
+
+example_makers = Dict{Type, Function}()
+
+"""
+Make a new example of the spoecified type.
+"""
+function example(t::Type{<:AbstractPanel})
+    example_makers[t]()
+end
+
+example_makers[WantedPanel] =
+    () -> WantedPanel(;
+                      label = "example WantedPanel",
+                      width = 6u"inch",
+                      length = 10u"inch")
+example_makers[FlippedPanel] =
+    () -> FlippedPanel(example(WantedPanel))
+example_makers[AvailablePanel] =
+    () -> AvailablePanel(;
+                         length = 4u"ft",
+                         width = 2u"ft",
+                         label = "example AvailablePanel",
+                         cost = 10u"USD")
+example_makers[BoughtPanel] =
+    () -> BoughtPanel(example(AvailablePanel))
+example_makers[Panel] =
+    () -> let
+        from = example(BoughtPanel)
+        axis = LengthAxis()
+        at = distance(from, axis) / 3
+        Panel(;
+              length = at,
+              width = distance(from, other(axis)),
+              cut_from = from,
+              cut_at = at,
+              cut_axis = axis,
+              x = from.x,
+              y = from.y,
+              cost = from.cost)
+    end
+example_makers[ScrappedPanel] =
+    () -> ScrappedPanel(; was=example(Panel))
+example_makers[FinishedPanel] =
+    () -> let
+        have = example(Panel)
+        FinishedPanel(have, WantedPanel(label = "want",
+                                        width = have.width,
+                                        length = have.length))
+    end
+
+@testset "PanelIdentity" begin
+    for paneltype in filter(isconcretetype, allsubtypes(AbstractPanel))
+        @test example(paneltype) != example(paneltype)
+    end
+end
+
+@testset "PanelIntegrity" begin
+    # Make sure we can't associate the wrong kinds or sizes of panels
+    # with one another
+end
+
 @testset "PanelCutting.jl" begin
     # Write your tests here.
 end
+
