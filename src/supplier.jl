60e4731247eb651f4a765b8b92fead9a18aa10ab
MarkNahabedian <naha@mit.edu>
2024-02-04 13:58:26 -0500

    Add a doc string.

--- src/supplier.jl
+++ src/supplier.jl
@@ -1,4 +1,14 @@
+export Supplier
 
+"""
+   Supplier(name::String, cost_per_cut, kerf, available_stock::Vector{AvailablePanel})
+
+A single supplier of sheet stock.
+
+Some suppliers will cut shet stock down to the customer's
+specifications.  `kerf` is the width of their saw custs.
+`cost_per_cut` is what they charge for each cut.
+"""
 @Base.kwdef mutable struct Supplier
     name::String
     cost_per_cut::MoneyType
